1d2140c31e02da1b4671dddf054a78e7eca055e6
brunoJorquera <jorquera.b18@gmail.com>
2021-08-23 13:18:37 -0700

    version 1

---
+++ intro.js
@@ -0,0 +1,31 @@
+console.log("Hackerman") 
+
+const firstName = "Bruno";
+let age = 21;
+age = 22;
+console.log(firstName, age);
+
+let favRobot = 'Optimus Prime';
+let favColor = `Red`;
+//concatenate strings with +
+console.log("Hello my name is " + firstName)
+//cpmcat wotj template literal
+console.log(`I am ${age} years old`)
+
+const isCool = true;
+
+let powerLevel;
+let weaknesses = null;
+console.log(powerLevel);
+
+if (favRobot = "Optimus Prime"){
+    console.log("That's my favorite too!")
+} else {
+    console.log("That's okay we can still be friends...")
+}
+
+if (age > 99){
+    console.log("Take away his driver's license")
+} else {
+    console.log("Move Along")
+} 
